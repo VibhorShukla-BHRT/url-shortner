b10f80a5398cec0491f2bf97a5f23ca99cdc67a6
VibhorShukla-BHRT <shukla.vibhor495@gmail.com>
2025-11-26 19:07:05 +0530

    added concurrency in backend

---
+++ backend/README.md
@@ -0,0 +1,2 @@
+### Free usage limits(change during prod):
+- Free version of avien postgres instance used with limit of 1GB(too small) -> change to aws s3
--- backend/src/services/shorten.ts
+++ backend/src/services/shorten.ts
@@ -1,27 +1,34 @@
 import { AppDataSource } from "../data-source";
 import { LAST, URL_ENTRY } from "../entity/URL_ENTRY";
 const mp: string[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
+
+//critical section
 async function next_perm(): Promise<string>{
-    const lastrepo = AppDataSource.getRepository(LAST);
-    let entity = await lastrepo.findOneBy({id: 1});
-    if(!entity){
-        entity = new LAST();
-        entity.id = 1;
-        entity.last = [0,0,0,0,0,0];
-        await lastrepo.save(entity);
-    }
-    let arr=entity.last;
-    for(let i=0;i<6;i++){
-        arr[i] = (arr[i]+1)%62;
-        if(arr[i]) break;
-    }
-    let res: string="";
-    for(let i=0;i<6;i++){
-        res+= mp[arr[i]];
-    }
-    entity.last = arr;
-    await lastrepo.save(entity);
-    return res;
+    return await AppDataSource.transaction(async (transactionalEntityManager)=>{
+        // const lastrepo = AppDataSource.getRepository(LAST);
+        let entity = await transactionalEntityManager.findOne(LAST,{
+            where: {id: 1},
+            lock: {mode: "pessimistic_write"}
+        });
+        if(!entity){
+            entity = new LAST();
+            entity.id = 1;
+            entity.last = [0,0,0,0,0,0];
+            await transactionalEntityManager.save(entity);
+        }
+        let arr=entity.last;
+        for(let i=0;i<6;i++){
+            arr[i] = (arr[i]+1)%62;
+            if(arr[i]) break;
+        }
+        let res: string="";
+        for(let i=0;i<6;i++){
+            res+= mp[arr[i]];
+        }
+        entity.last = arr;
+        await transactionalEntityManager.save(entity);
+        return res;
+    });
 }
 //encoder
 export async function shorten_url(url: string): Promise<string>{
